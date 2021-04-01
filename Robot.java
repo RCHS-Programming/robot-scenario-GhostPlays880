52cef80616b1bc0561fb3193896d31ea329adba5
GhostPlays880 <80477077+GhostPlays880@users.noreply.github.com>
2021-04-01 12:29:08 -0400

    added pre-built structures

--- Robot.java
+++ Robot.java
@@ -14,6 +14,34 @@ public class Robot extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        Movement();
     }    
+    public void Movement()
+    {
+        if(Greenfoot.isKeyDown ("up"))
+        {
+            setLocation( getX(), getY() -3);
+        }
+        if(Greenfoot.isKeyDown ("down"))
+        {
+            setLocation( getX(), getY() +3);
+        }
+         if(Greenfoot.isKeyDown ("left"))
+        {
+            setLocation( getX() -3, getY() );
+        }
+         if(Greenfoot.isKeyDown ("right"))
+        {
+            setLocation( getX() +3, getY() );
+        }
+    
+    }
+    public void detectBlockCollision()
+    {
+       if (isTouching(Wall.class))
+       {
+           
+       }
+    }
+    
 }
--- RobotWorld.java
+++ RobotWorld.java
@@ -17,5 +17,19 @@ public class RobotWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        addObject (new Robot(), 35, 50);
+        addObject (new Wall(), 750, 200);
+        addObject (new Wall(), 650, 200);
+        addObject (new Wall(), 0, 200);
+        addObject (new Wall(), 100, 200);
+        addObject (new Wall(), 200, 200);
+        addObject (new Wall(), 300, 200);
+        addObject (new Wall(), 350, 200);
+        addObject (new Block(), 500, 200);
+        addObject (new Pizza(), 500, 300);
+        addObject (new Pizza(), 500, 350);
+        addObject (new Pizza(), 500, 400);
+        addObject (new Pizza(), 500, 450);
+        addObject (new Pizza(), 500, 500);
     }
 }
